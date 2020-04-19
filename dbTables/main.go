e7f03474a45c357bff1172bcc6945d59f14ca7cc
keepcalmist <k.unshikov2010@icloud.com>
2020-04-19 19:43:50 +0300

    first commit of tables creator

---
+++ dbTables/main.go
@@ -0,0 +1,39 @@
+package dbTables
+
+import (
+	"LinksParser/logger"
+	"fmt"
+	"os"
+)
+
+func WriteTable(name string, table map[string]string) {
+	var flag bool = true
+	file, err := os.OpenFile("tables.go", os.O_CREATE, 0777)
+	if err != nil {
+		logger.FindEr(err)
+		flag = false
+	}
+
+	if flag == false {
+		file, err = os.OpenFile("tables.go", os.O_APPEND, 0777)
+
+		if err != nil {
+			logger.FindEr(err)
+			os.Exit(1)
+		}
+	}
+	if flag == true {
+		fmt.Fprintln(file, "package main")
+		fmt.Fprintln(file)
+	}
+	_, err = fmt.Fprintln(file, "type ", name, " struct { ")
+	if err != nil {
+		logger.FindEr(err)
+	}
+
+	for key, ex := range table {
+		_, err = fmt.Fprintln(file, "\t", key, " ", ex)
+	}
+	_, err = fmt.Fprintln(file, "}")
+	_ = file.Close()
+}
